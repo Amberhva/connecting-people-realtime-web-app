55ef5ff19466753af185ec950e1433f43e81d20d
zenitba <112856019+zenitba@users.noreply.github.com>
2023-05-26 22:41:31 +0200

    extra tools voor de chatroom

--- public/chat.js
+++ public/chat.js
@@ -11,35 +11,35 @@ inputField.addEventListener("blur", function () {
 
 let socket = io()
 let messages = document.querySelector("section ul")
-let input = document.querySelector("input")
+let input = document.querySelector('#message')
+let handle = document.querySelector('#handle')
 let count =  document.querySelector('#count');
-
 document.querySelector("form").addEventListener("submit", (event) => {
-    event.preventDefault()
-    if (input.value) {
-      socket.emit("message", input.value)
-      input.value = ""
-    }
-  })
+  event.preventDefault()
+  if (input.value) {
+    socket.emit("message", input.value)
+    input.value = ""
+  }
+})
 
 socket.on("message", (message) => {
-  addMessage(message)
+addMessage(message)
 })
 
 socket.on("whatever", (message) => {
-  addMessage(message)
+addMessage(message)
 })
 
 socket.on("history", (history) => {
-  history.forEach((message) => {
-    addMessage(message)
-  })
+history.forEach((message) => {
+  addMessage(message)
+})
 })
 socket.on('usercount', data => {
-    count.innerHTML = data;
-  })
+  count.innerHTML = data;
+})
 
 function addMessage(message) {
-  messages.appendChild(Object.assign(document.createElement("li"), { textContent: message }))
-  messages.scrollTop = messages.scrollHeight;
+messages.appendChild(Object.assign(document.createElement("li"), { textContent: message }))
+messages.scrollTop = messages.scrollHeight;
 }
